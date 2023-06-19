ae93f67314722495ee3187fa5a2c7a4d2360b894
Lami <Halam.Lee.Lami@gmail.com>
2023-06-19 19:16:10 +0900

    setting: 글로벌 폰트 Noto Sans Korean 설정 #4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import notoSansKr from '@/styles/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={notoSansKr.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
