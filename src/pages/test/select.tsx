11f820de5745c9214a3ec895c48f272f62caa932
Lami <Halam.Lee.Lami@gmail.com>
2023-06-23 03:56:18 +0900

    test: Select 컴포넌트 수정사항에 따른 테스트 변경 #18

--- src/pages/test/select.tsx
+++ src/pages/test/select.tsx
@@ -1,18 +1,28 @@
 import { styled } from 'styled-components';
 import CustomSelect from '@components/select';
+import { useEffect, useState } from 'react';
 
 const Select = () => {
+  const [test1, setTest1] = useState('');
+  const [test2, setTest2] = useState('');
+
+  useEffect(() => {
+    console.log({ test1, test2 });
+  }, [test1, test2]);
+
   return (
     <Wrapper>
       <CustomSelect
         width={'200px'}
         initialText={'테스트'}
         optionData={['test', 'test2']}
+        setSelect={(selected: string) => setTest1(selected)}
       />
       <CustomSelect
         width={'400px'}
         initialText={'테스트'}
         optionData={['test', 'test2']}
+        setSelect={(selected: string) => setTest2(selected)}
       />
     </Wrapper>
   );
