e2d82e5a4c1c3f26380136eb95ef55d0197d5ded
Lami <Halam.Lee.Lami@gmail.com>
2023-06-23 23:47:42 +0900

    design: 기본 폰트 설정 #29

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -7,6 +7,12 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  
+  * {
+    font-family: 'TheJamsil5Bold', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
 `;
 
 export default GlobalStyle;
