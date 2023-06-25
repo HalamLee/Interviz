3cf4ce68ba3caf3a148650f158aae62902bd2406
Lami <Halam.Lee.Lami@gmail.com>
2023-06-25 22:50:54 +0900

    design: question 페이지 디자인 검토 후 수정 #46

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -14,10 +14,14 @@ export default Question;
 
 const Wrapper = styled.div`
   ${theme.shadows.small};
-  width: 80%;
+  width: 75%;
   border-radius: 8px;
   padding: 16px 24px;
   word-break: keep-all;
   line-height: 160%;
   cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    transition: all 0.3s ease;
+  }
 `;
