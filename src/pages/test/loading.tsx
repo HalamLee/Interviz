184f9d542e844b518287d8fb320623007323e822
Lami <Halam.Lee.Lami@gmail.com>
2023-06-25 15:11:29 +0900

    test: 로딩 컴포넌트 수정사항 반영 #41

--- src/pages/test/loading.tsx
+++ src/pages/test/loading.tsx
@@ -1,7 +1,8 @@
 import Loading from '@components/loading/Loading';
 
 const LoadingTest = () => {
-  return <Loading />;
+  return <Loading size={'small'} width="40px" />;
+  // return <Loading size={'big'} width="100%" />;
 };
 
 export default LoadingTest;
